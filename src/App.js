95dde50a223e8fab059b1b6eccda7d6f4b9c463a
unknown <CoreyTyler>
2020-10-05 15:39:44 -0500

    login-page-first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-// import './App.css';
+import React from "react";
+import "./App.css";
 // import { Typography } from '@material-ui/core';
-import Header from './components/Header'
+import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Header />
-    
+      <div className="Login">
+        <Login />
+      </div>
     </div>
   );
 }
